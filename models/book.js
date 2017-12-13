82d368d589888f96c327c9bde4ebf80ceb79afa0
Sherif Ismail <sherifismail@proslamx.local>
2017-12-13 12:25:06 -0800

    created book model

---
+++ models/book.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var BookSchema = new Schema({
+	title: String,
+	author: String,
+	image: String,
+	release_date: String 
+});
+
+var Book = mongoose.model('Book', BookSchema);
+
+
+
+
+
+
+
+
+
+var book = mongoose.model('Book', BookSchema);
+
+module.exports = Book;
--- models/index.js
+++ models/index.js
@@ -1,2 +1,9 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/book-app");
+
+
+var Book = require('./book.js');
+var author = require('./author.js');
+
+exports.Book = Book;
+exports.author = author;
